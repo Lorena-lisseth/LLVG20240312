08eed53efe9df7269b60ac197558a9a19324a685
Lorena Lisseth Vasquez Godoy <2431186@clases.edu.sv>
2024-03-12 21:30:25 -0600

    Se agrego el script

---
+++ LLVG20240312/Models/Script.sql
@@ -0,0 +1,20 @@
+-- Creación de la base de datos LRVG20241103DB
+CREATE DATABASE LLVG20241103DB;
+USE LLVG20241103DB;
+
+CREATE TABLE Clientes (
+    id_cliente INT IDENTITY(1, 1) PRIMARY KEY,
+    nombre VARCHAR(200),
+    direccion VARCHAR(255),
+    correo_electronico VARCHAR(100),
+);
+
+-- Tabla de Números de Teléfono
+CREATE TABLE NumerosTelefonos (
+    id_telefono INT IDENTITY(1, 1) PRIMARY KEY,
+    id_cliente INT,
+    numero_telefono VARCHAR(9),
+    tipo_telefono VARCHAR(50), -- Ejemplo: "Casa", "Trabajo", "Móvil", etc.
+    -- Otros campos adicionales que desees agregar
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente)
+);
