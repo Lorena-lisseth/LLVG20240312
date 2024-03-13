4822dddf1c3a02fc91f839fab29a7b958b72b80c
Lorena Lisseth Vasquez Godoy <2431186@clases.edu.sv>
2024-03-12 20:37:44 -0600

    Agregar archivos de proyecto.

---
+++ LLVG20240312/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LLVG20240312.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
