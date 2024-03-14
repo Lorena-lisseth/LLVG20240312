4f963ae1602e9f01936813e954538c568e7e3952
Lorena Lisseth Vasquez Godoy <2431186@clases.edu.sv>
2024-03-14 11:24:10 -0600

    cambios en la vista crear

--- LLVG20240312/Controllers/ClientesController.cs
+++ LLVG20240312/Controllers/ClientesController.cs
@@ -35,19 +35,26 @@ namespace LLVG20240312.Controllers
             }
 
             var cliente = await _context.Clientes
+                 .Include(s => s.NumerosTelefono)
                 .FirstOrDefaultAsync(m => m.IdCliente == id);
             if (cliente == null)
             {
                 return NotFound();
             }
-
+            ViewBag.Accion = "Details";
             return View(cliente);
         }
 
         // GET: Clientes/Create
         public IActionResult Create()
         {
-            return View();
+            var cliente = new Cliente();
+            cliente.NumerosTelefono = new List<NumerosTelefono>();
+            cliente.NumerosTelefono.Add(new NumerosTelefono
+            {
+            });
+            ViewBag.Accion = "Create";
+            return View(cliente);
         }
 
         // POST: Clientes/Create
@@ -55,7 +62,7 @@ namespace LLVG20240312.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdCliente,Nombre,Direccion,CorreoElectronico")] Cliente cliente)
+        public async Task<IActionResult> Create([Bind("IdCliente,Nombre,Direccion,CorreoElectronico, NumerosTelefono")] Cliente cliente)
         {
             if (ModelState.IsValid)
             {
@@ -65,7 +72,27 @@ namespace LLVG20240312.Controllers
             }
             return View(cliente);
         }
+        public ActionResult AgregarDetalles([Bind("Id,Nombre,Apellido,NumerosTelefono")] Cliente cliente, string accion)
+        {
+            cliente.NumerosTelefono.Add(new NumerosTelefono { });
+            ViewBag.Accion = accion;
+            return View(accion, cliente);
+        }
+        public ActionResult EliminarDetalles([Bind("Id,Nombre,Apellido,NumerosTelefono")] Cliente cliente, int index, string accion)
+        {
+            var det = cliente.NumerosTelefono[index];
+            if (accion == "Edit" && det.IdTelefono > 0)
+            {
+                det.IdTelefono = det.IdTelefono * -1;
+            }
+            else
+            {
+                cliente.NumerosTelefono.RemoveAt(index);
+            }
 
+            ViewBag.Accion = accion;
+            return View(accion, cliente);
+        }
         // GET: Clientes/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
